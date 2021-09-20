84c837956fb75ddaf25a76bf97d785f3c00b66b6
Preffet <jstmtr@gmail.com>
2021-09-20 17:58:05 +0100

    Added a way of returning the list with open ports

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,21 @@
 import socket
 
-def get_open_ports(target, port_range):
+def get_open_ports(target, port_range, verbose=False):
     open_ports = []
+    # IPv4 TCP connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = target
+
+    #if verbose mode is set to False, return a list of open ports:
+    if(verbose == False):
+        #check all ports in the given range
+        for i in range(port_range[0], port_range[1]+1):
+            if s.connect_ex((host, i)):
+                print(str(i)+" port is closed")
+            else:
+                open_ports.append(i)
+                print(str(i)+" port is open")
+        return(open_ports)
 
 
 
-    return(open_ports)
